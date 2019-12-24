ac61073dbc4ad67f2907c1460848f84e377f38e0
gaurav <gaurav@home>
2019-12-24 20:23:47 +0530

    added certificate extractor batch

---
+++ extract_publisher_cert.bat
@@ -0,0 +1,8 @@
+keytool -export ^
+-rfc ^
+-file newsfeed.cert ^
+-alias tomcat ^
+-storetype JKS ^
+-storepass changeit ^
+-keypass changeit ^
+-keystore C:\Users\gaurav\eclipse-workspace\Servers\Tomcat v8.5 Server at localhost-config\keystore
