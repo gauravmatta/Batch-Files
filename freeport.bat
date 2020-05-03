0c776932da7423c44c3a117f3244851fefc519f9
Gaurav Matta <gaurav.matta@gmail.com>
2020-05-03 16:31:32 +0530

    Updated Free Port to ask and look port and delete process
    Signed-off-by: Gaurav Matta <gaurav.matta@gmail.com>

--- freeport.bat
+++ freeport.bat
@@ -1,2 +1,6 @@
-FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8080') DO @ECHO TaskKill.exe /F /PID %%P
-FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8009') DO @ECHO TaskKill.exe /F /PID %%P
+@echo off
+set /p port= "Please Enter the port of the process to end :"
+echo To End Process for %port% please execute in Admin Mode
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :%port%') DO echo TaskKill.exe /F /PID %%P
+REM FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8080') DO echo TaskKill.exe /F /PID %%P
+REM FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8009') DO echo TaskKill.exe /F /PID %%P
